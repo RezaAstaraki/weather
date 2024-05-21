f907e21c3dcbe3841c21d8250e7e5c5cdebb89df
RezaAstarki <reza.astaraky@gmail.com>
2024-05-21 19:39:44 +0330

    use state management for theme

--- app/utils/client side utils/ThemeProvider.tsx
+++ app/utils/client side utils/ThemeProvider.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { dayOrNightTheme } from "../convertors";
 
 const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const [theme, setTheme] = useState("");
+  useEffect(() => {
+    setTheme(dayOrNightTheme());
+  });
   return (
     <div
       style={{
-        background: dayOrNightTheme(),
+        background: theme,
       }}
     >
       {children}
